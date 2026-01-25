b967bfed9d5de8b8a8307dacb84bbb3e54c8098e
prashantmore45 <prashantmorepm05@gmail.com>
2026-01-26 00:51:34 +0530

    Added implementation of findErrorNums function to identify duplicate and missing numbers in an integer array

---
+++ 15. Vectors/Assignments/assignment1.cpp
@@ -0,0 +1,51 @@
+/* 
+Question 1 : You have a set of integers, which originally contains all the numbers 
+from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated 
+to another number in the set, which results in repetition of one number and loss of 
+another number. 
+
+You are given an integer array nums representing the data status of this set after the 
+error. 
+
+Find the number that occurs twice and the number that is missing and return them in 
+the form of an array.
+
+Example : Input: nums = [1,2,2,4] ; Output: [2,3] 
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> findErrorNums(vector<int>& nums) {
+    vector<int> ans; 
+    int n = nums.size(); 
+    vector<bool> isPresent(n+1, false); 
+    
+    for(int i=0; i<n; i++) { 
+        if(isPresent[nums[i]]) { 
+            ans.push_back(nums[i]); 
+        } 
+        else { 
+            isPresent[nums[i]] = true; 
+        }
+    } 
+    
+    for(int i=1; i<isPresent.size(); i++) { 
+        if(!isPresent[i]) { 
+            ans.push_back(i); 
+            break; 
+        } 
+    } 
+    
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 2, 4};
+    vector<int> ans = findErrorNums(nums);
+    
+    cout << "Output: [" << ans[0] << ", " << ans[1] << "]" << endl; // Output: [2, 3]
+    
+    return 0;
+}
