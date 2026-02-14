a2b34f3df930f37f6e9176c3e411af7ef70ff84d
prashantmore45 <prashantmorepm05@gmail.com>
2026-02-15 00:31:47 +0530

    Add implementation of recursive function to calculate the sum of first N numbers

---
+++ 19. Recursion (Part 01)/Sum_of_N_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n) {
+   if (n == 1) {
+    return 1;
+   }
+   return n + sum(n-1);
+}
+
+int main() {
+    int ans = sum(5);
+    cout << ans << endl;
+    return 0;
+}
