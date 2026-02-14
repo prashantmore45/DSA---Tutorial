29d7f32ace284d26f80ecd3584070eec461e1aae
prashantmore45 <prashantmorepm05@gmail.com>
2026-02-14 22:45:32 +0530

    Add implementation of recursive function to print numbers in decreasing order

---
+++ 19. Recursion (Part 01)/example1.cpp
@@ -0,0 +1,17 @@
+//Print numbers in decreasing order using recursion
+
+#include<iostream>
+using namespace std;
+
+void print(int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << n << " ";
+    print(n - 1);
+}
+
+int main() {
+    print(100);
+    return 0;
+}
