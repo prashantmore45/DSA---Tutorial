2605ac3a7198d09d2d0bf690f8241ab91c033cfb
prashantmore45 <prashantmorepm05@gmail.com>
2026-02-15 00:59:10 +0530

    Add implementation of recursive function to find the first occurrence of a target in an array

---
+++ 19. Recursion (Part 01)/FirstOccurence.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int firstOccurence(vector<int> arr, int i, int target) {
+    if( i == arr.size() ) {
+        return -1;
+    }
+
+    if (arr[i] == target) {
+        return i;
+    }
+
+    return firstOccurence(arr, i+1, target);
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 3, 3, 4};
+    cout << firstOccurence(arr, 0, 3) << endl;
+    return 0;
+}
