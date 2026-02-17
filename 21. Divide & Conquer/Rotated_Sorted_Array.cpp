ad42a907b9e3ac99b245d0ff8448f95b0d979d4e
prashantmore45 <prashantmorepm05@gmail.com>
2026-02-17 12:21:16 +0530

    Add implementation of search function in rotated sorted array

---
+++ 21. Divide & Conquer/Rotated_Sorted_Array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int search(int arr[], int si, int ei, int target) {
+    //Base Case
+    if (si > ei) {
+        return -1;
+    }
+
+    int mid = (si + ei) / 2;
+
+    if (arr[mid] == target) {
+        return mid;
+    }
+
+    if (arr[si] <= arr[mid]) { //L1
+        if (arr[si] <= target && target <= arr[mid]) {
+            search(arr, si, mid-1, target); //left half
+        } else {
+            search(arr, mid+1, ei, target); //right half
+        }
+    } else { //L2
+        if (arr[mid] <= target && target <= arr[ei]) {
+            search(arr, mid+1, ei, target); //right half
+        } else {
+            search(arr, si, mid-1, target); //left half
+        }
+    }
+}
+
+int main() {
+    int arr[7] = {4, 5, 6, 7, 0, 1, 2};
+    int n = 7;
+
+    cout << "Idx: " << search(arr, 0, n-1, 0) << endl; //4
+    return 0;
+}
