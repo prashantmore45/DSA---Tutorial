632aabe5c064b67a19bd6e712ce9c0bb9891b1d1
prashantmore45 <prashantmorepm05@gmail.com>
2026-02-20 11:44:02 +0530

    Add implementation of linear time complexity example in C++

---
+++ 22. Time & Space Complexity (Part 01)/Linear_TC.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    for (int i = 0; i < n; i++)  // This loop iterates through each element of the array, resulting in a linear time complexity O(n) since it depends on the size of the array.
+    {
+        cout << arr[i] << " "; 
+    }
+    cout << endl;
+    
+    return 0;
+}
