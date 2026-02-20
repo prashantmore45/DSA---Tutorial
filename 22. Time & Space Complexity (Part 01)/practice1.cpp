ca3bb80dcab909e2cf19fe2825adb9469ff5b74a
prashantmore45 <prashantmorepm05@gmail.com>
2026-02-20 11:52:16 +0530

    Add implementation of nested loop for unique pairs with O(n^2) time complexity

---
+++ 22. Time & Space Complexity (Part 01)/practice1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) 
+        {
+            cout << arr[i] << " " << arr[j] << endl; // This nested loop iterates through all unique pairs of elements in the array, resulting in a time complexity of O(n^2) since it depends on the size of the array.
+        }
+        
+    }
+
+    return 0;
+}
