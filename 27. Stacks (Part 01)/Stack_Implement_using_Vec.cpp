c8bb1665f5fb3bed49bb0fd21751e07978c2e20e
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-04 18:18:02 +0530

    Add implementation for stack using vector

---
+++ 27. Stacks (Part 01)/Stack_Implement_using_Vec.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack {
+        vector<int> vec;
+    public:
+        void push(int val) {  // O(1)
+            vec.push_back(val);
+        }
+
+        void pop() {    // O(1)
+            if (isEmpty()) {
+                cout << "Stack is empty.\n";
+                return;
+            }
+
+            vec.pop_back();
+        }
+
+        int top() {  // O(1)
+            if (isEmpty()) {
+                cout << "Stack is empty.\n";
+                return -1;
+            }
+
+            int lastIdx = vec.size() - 1;
+            return vec[lastIdx];
+        }
+
+        bool isEmpty() {
+            return vec.size() == 0;
+        }
+        
+};
+
+int main() {
+    Stack s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    while (!s.isEmpty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
