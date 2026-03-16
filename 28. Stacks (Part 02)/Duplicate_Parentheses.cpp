febe77fcfc41eb83300d057ee9971712ea5d18c2
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-16 23:31:34 +0530

    Add implementation for duplicate parentheses check using stack

---
+++ 28. Stacks (Part 02)/Duplicate_Parentheses.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isDuplicate(string str) {
+    stack<char> s;
+
+    for (int i = 0; i < str.size(); i++) {
+        char ch = str[i];
+
+        if (ch != ')') {  //non-closing
+            s.push(ch);
+        } else {  //closing 
+            if (s.top() == '(') {
+                return true; //DUPLICATE
+            }
+
+            while (s.top() != '(') {
+                s.pop();
+            }
+            s.pop();
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    string str1 = "((a+b))"; //Invalid : true
+    string str2 = "((a+b)+(c+d))"; //Valid: false
+
+    cout << isDuplicate(str1) << endl;
+    cout << isDuplicate(str2) << endl;
+
+    return 0;
+}
