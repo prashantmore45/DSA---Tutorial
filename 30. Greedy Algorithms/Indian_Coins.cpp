5cedb8671429c439e036168e47753b447cc37add
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-20 22:23:25 +0530

    Add implementation for minimum coins change problem using greedy approach

---
+++ 30. Greedy Algorithms/Indian_Coins.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int getMinChange(vector<int> coins, int V) {
+    int n = coins.size();
+    int ans = 0;
+
+    for (int i=n-1; i>=0 && V>0; i--) {
+        if (V >= coins[i]) {
+            ans += V / coins[i];
+            V = V % coins[i];
+        }
+    }
+
+    cout << "min coins for change = " << ans << endl;
+    return ans;
+}
+
+int main() {
+    vector<int> coins = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+    int V = 590;
+
+    getMinChange(coins, V);
+    return 0;
+}
