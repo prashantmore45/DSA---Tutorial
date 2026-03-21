a374bb3f46b58da498ed523261e3ded9189d83ee
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-22 02:03:53 +0530

    Add implementation for calculating the height of a binary tree

---
+++ 31. Binary Trees/height_of_tree.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> nodes) {
+    idx++;
+    
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+}
+
+int height(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
+
+    int currHt = max(leftHt, rightHt) + 1;
+
+    return currHt;
+}
+
+int main() {
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Node* root = buildTree(nodes);
+
+    cout << "height = " << height(root) << endl;  // 3
+
+    // vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, 6, -1, 7, -1, -1, 3, -1, -1};
+    // Node* root1 = buildTree(nodes);
+
+    // cout << "height = " << height(root1) << endl; // 5
+    return 0;
+}
