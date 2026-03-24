01559b98a51c21ce39ffc16bb667947db38478b0
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-25 01:24:37 +0530

    Add implementation for deleting a node in a binary search tree

---
+++ 34. BST(Part 01)/delete_node_BST.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public: 
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == NULL) {
+        Node* root = new Node(val);
+        return root;
+    }
+
+    if (root->data > val) {  // left subtree
+        root->left = insert(root->left, val);
+    } else {  // right subtree
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+Node* buildBST(int arr[], int n) {
+    Node* root = NULL;
+
+    for (int i=0; i<n; i++) {
+        root = insert(root, arr[i]);
+    }
+
+    return root;
+}
+
+Node* getInorderSuccessor(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+
+    return root;
+}
+
+Node* delNode(Node* root, int key) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (root->data > key) { //left subtree
+        root->left = delNode(root->left, key);
+    } else if (root->data < key) {  //right subtree
+        root->right = delNode(root->right, key);
+    } else {
+        //root = key
+        //case1: 0 children
+        if (root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+
+        //case2: 1 child
+        if (root->left == NULL || root->right == NULL) {
+            return root->left == NULL ? root->right : root->left;
+        }
+
+        //case3: 2 child
+        Node* IS = getInorderSuccessor(root->right);
+        root->data = IS->data;
+        root->right = delNode(root->right, IS->data); // case1, case2
+        return root;
+    }
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    int arr[9] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+    Node* root = buildBST(arr, 9);
+
+    inorder(root);
+    cout << endl;
+
+    delNode(root, 5);
+
+    inorder(root);
+    cout << endl;
+    return 0;
+}
