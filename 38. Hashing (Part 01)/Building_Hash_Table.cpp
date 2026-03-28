21ccc522100ee4506016005b15c4ea83bc6cbd05
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-29 03:17:10 +0530

    Add implementation of Hash Table with basic insert functionality

---
+++ 38. Hashing (Part 01)/Building_Hash_Table.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    string key;
+    int val;
+    Node* next;
+
+    Node(string key, int val) {
+        this->key = key;
+        this->val = val;
+        next = NULL;
+    }
+};
+
+class HashTable {
+    int totalSize;
+    int currSize;
+    Node** table;
+
+    int HashFunction(string key) {
+        int idx = 0;
+
+        for (int i=0; i<key.size(); i++) {
+            idx = idx + (key[i]*key[i]) % key.size();
+        }
+
+        return idx;
+    }
+
+public:
+    HashTable(int size = 5) {
+        this->totalSize = size;
+
+        table = new Node*[totalSize];
+
+        for (int i=0; i<totalSize; i++) {
+            table[i] = NULL;
+        }
+    }
+
+    void insert(string key, val) {
+        int idx = HashFunction(key);
+
+        Node* newNode = new Node(key, val);
+        Node* head = NULL;
+
+        newNode->next = head;
+        head = newNode;
+
+        currSize++;
+    }
+};
+
+
+int main() {
+    HashTable ht;
+    return 0;
+}
