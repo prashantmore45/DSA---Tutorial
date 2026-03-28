4658c0dcf3b30005ba2d819f610cc5c37dcb6534
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-29 04:38:25 +0530

    Implement remove method in HashTable to delete nodes by key

--- 38. Hashing (Part 01)/Building_Hash_Table.cpp
+++ 38. Hashing (Part 01)/Building_Hash_Table.cpp
@@ -121,6 +121,28 @@ public:
         return -1;
     }
 
+    void remove(string key) {
+        int idx = HashFunction(key);
+
+        Node* temp = table[idx];
+        Node* prev = temp;
+
+        while (temp != NULL) { 
+            if (temp->key == key) {  //erase
+                if (prev == temp) {  //head
+                    table[idx] = temp->next; 
+                } else {
+                    prev->next = temp->next;
+                }
+                break;
+            }
+
+            prev = temp;
+            temp = temp->next;
+
+        }
+    }
+
     void print() {
 
         for (int i=0; i<totalSize; i++) {
@@ -148,6 +170,16 @@ int main() {
     //     cout << "India population : " << ht.search("India") << endl;
     // }
 
+    ht.print();
+
+    cout << "-----------\n";
+
+    ht.remove("China");
+    ht.print();
+
+    cout << "-----------\n";
+
+    ht.remove("US");
     ht.print();
 
     return 0;
