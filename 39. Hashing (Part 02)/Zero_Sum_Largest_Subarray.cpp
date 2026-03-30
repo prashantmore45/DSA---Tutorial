44d3edd2b32cbb15cd68d1aaf633d1a3351b023c
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-30 23:33:13 +0530

    Add Largest Subarray with Zero Sum example demonstrating usage of unordered_map

---
+++ 39. Hashing (Part 02)/Zero_Sum_Largest_Subarray.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int largestSubWith0Sum(vector<int> arr) {
+    unordered_map<int, int> m;
+
+    int sum = 0;
+    int ans = 0;
+
+    for (int j=0; j<arr.size(); j++) {
+        sum += arr[j];
+
+        if (m.count(sum)) {
+            int currLen = j - m[sum]; // j - idx
+            ans = max(ans, currLen);
+        } else {
+            m[sum] = j;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {15, 2, -2, -8, 1, 7, 10};
+
+    cout << "Largest subarry length with sum 0 : " << largestSubWith0Sum(arr) << endl;
+    return 0;
+}
