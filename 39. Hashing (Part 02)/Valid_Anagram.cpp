1c8289135ba32e22a7c03f6825a3c542e3133a84
prashantmore45 <prashantmorepm05@gmail.com>
2026-03-30 16:40:18 +0530

    Add Valid Anagram example demonstrating character frequency comparison using unordered_map

---
+++ 39. Hashing (Part 02)/Valid_Anagram.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+bool validAnagrams(string s, string t) {
+    if (s.size() != t.size()) {
+        return false;
+    }
+
+    //s <char, freq>
+    unordered_map<char, int> freq;
+
+    for (int i=0; i<s.size(); i++) {
+        if (freq.count(s[i])) {
+            freq[s[i]]++;
+        } else {
+            freq[s[i]] = 1;
+        }
+    }
+
+    for (int i=0; i<t.size(); i++) {
+        if (freq.count(t[i])) {
+            freq[t[i]]--;
+            if (freq[t[i]] == 0) {
+                freq.erase(t[i]);
+            }
+        } else {
+            return false;
+        }
+    }
+
+    return freq.size() == 0;
+}
+
+
+int main() {
+    string s = "race";
+    string t = "care";
+
+    cout << validAnagrams(s, t) << endl; //1
+    return 0;
+}
