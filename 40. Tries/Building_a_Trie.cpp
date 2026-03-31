573839653ac6361f65f3082d7c56481fd4fb9cf6
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-01 00:20:31 +0530

    feat: implement Trie data structure with insert functionality

---
+++ 40. Tries/Building_a_Trie.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Node {
+public:
+    unordered_map<char, Node*> children;
+    bool endOfWord;
+
+    Node() {
+        endOfWord = false;
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string key) {
+        Node* temp = root;
+
+        for (int i=0; i<key.length(); i++) {
+            if (temp->children.count(key[i] == 0)) {
+                temp->children[key[i]] = new Node();  //insert
+            }
+            temp = temp->children[key[i]];
+        }
+
+        temp->endOfWord = true;
+    }
+};
+
+
+int main() {
+    vector<string> words = {"the", "a", "there", "their", "any", "thee"};
+    Trie trie;
+
+    for (int i=0; i<words.size(); i++) {
+        trie.insert(words[i]);
+    }
+
+    return 0;
+}
