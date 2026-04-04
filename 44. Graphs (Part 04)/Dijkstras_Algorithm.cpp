ea33e27054234e22fd122b859c77369bd46d1253
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-05 03:36:39 +0530

    feat: implement Dijkstra's algorithm for shortest path in a weighted graph

---
+++ 44. Graphs (Part 04)/Dijkstras_Algorithm.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<climits>
+using namespace std;
+
+class Edge {
+public:
+    int v;
+    int wt;
+
+    Edge(int v, int wt) {
+        this->v = v;
+        this->wt = wt;
+    }
+};
+
+void dijkstras(int src, vector<vector<Edge>> graph, int V) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //min Heap
+    //pair<dist[v], v> 
+    vector<int> dist(V, INT_MAX);
+
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    while (pq.size() > 0) {
+        int u = pq.top().second;
+        pq.pop();
+
+        vector<Edge> edges = graph[u];
+
+        for (Edge e : edges) { //e.v, e.wt
+            if (dist[e.v] > dist[u] + e.wt) {
+                dist[e.v] = dist[u] + e.wt;
+                pq.push(make_pair(dist[e.v], e.v));
+            }
+        }
+    }
+
+    //print dist
+    for (int d : dist) {
+        cout << d << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int V = 6;
+    vector<vector<Edge>> graph(V);
+    
+    graph[0].push_back(Edge(1, 2));
+    graph[0].push_back(Edge(2, 4));
+
+    graph[1].push_back(Edge(2, 1));
+    graph[1].push_back(Edge(3, 7));
+
+    graph[2].push_back(Edge(4, 3));
+
+    graph[3].push_back(Edge(5, 1));
+
+    graph[4].push_back(Edge(3, 2));
+    graph[4].push_back(Edge(5, 5));
+
+    dijkstras(0, graph, V); //0 2 3 8 6 9 
+    dijkstras(1, graph, V); //2147483647(INT_MAX) 0 1 6 4 7 
+    return 0;
+}
