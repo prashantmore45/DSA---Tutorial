99fb084754d745be436be5406ecd61e3fc3b5567
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-07 00:18:09 +0530

    feat: refactor findCheapestPrice function to improve parameter handling and distance tracking

--- 45. Graphs (Part 05)/Cheapest_Flights_within_K_Stops.cpp
+++ 45. Graphs (Part 05)/Cheapest_Flights_within_K_Stops.cpp
@@ -17,34 +17,43 @@ public:
     }
 };
 
-int findCheapestPrice(vector<vector<int>> &flights, int src, int dest, int k) {
-    int V = flights.size();
+int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dest, int k) {
     queue<Info> q;
-    vector<int> dist(V, INT_MAX);
+    vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
 
-    dist[src] = 0;
-    q.push(src, 0, -1);
+    dist[src][0] = 0;
+    q.push(Info(src, 0, -1));
 
     while (q.size() > 0) {
         Info curr = q.front();
         q.pop();
 
-        for (int i=0; i<V; i++) {
+        for (int i=0; i<flights.size(); i++) {
             if (flights[i][0] == curr.u) {
                 int v = flights[i][1];
                 int wt = flights[i][2];
 
-                if (dist[v] > curr.cost + wt && curr.stops+1 <= k) {
-                    dist[v] = curr.cost + wt;
-                    q.push(v, dist[v], curr.stops+1);
+                if (curr.stops + 1 <= k + 1 &&
+                    dist[v][curr.stops + 1] > curr.cost + wt) {
+
+                    dist[v][curr.stops + 1] = curr.cost + wt;
+                    q.push(Info(v, dist[v][curr.stops + 1], curr.stops + 1));
                 }
             }
         }
     }
 
-    if (dist[dest] == INT_MAX) {
-        return -1;
+    int ans = INT_MAX;
+    for (int i = 0; i <= k+1; i++) {
+        ans = min(ans, dist[dest][i]);
     }
 
-    return dist[dest];
+    return ans == INT_MAX ? -1 : ans;
+}
+
+int main() {
+    vector<vector<int>> flights = {{0,1,100},{1,2,100},{0,2,500}};
+    int n = 3;
+
+    cout << findCheapestPrice(n, flights, 0, 2, 1);
 }
