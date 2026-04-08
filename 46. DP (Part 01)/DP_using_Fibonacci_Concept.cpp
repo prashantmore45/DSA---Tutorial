39a8d6d931ffc339c3ee6dd690949db2c2becbf7
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-09 00:08:26 +0530

    feat: add tabulation method for Fibonacci sequence calculation

--- 46. DP (Part 01)/DP_using_Fibonacci_Concept.cpp
+++ 46. DP (Part 01)/DP_using_Fibonacci_Concept.cpp
@@ -19,11 +19,25 @@ int fibDP(int n, vector<int> &f) {  //O(n)
 }
 
 
+int fibTab(int n) {  //O(n)
+    vector<int> fib(n+1, 0);
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i=2; i<=n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    return fib[n];
+}
+
+
 int main() {
     int n = 6;
     vector<int> f(n+1, -1);
 
     cout << fibDP(n, f) << endl;
-
+    cout << fibTab(n) << endl;
+    
     return 0;
 }
