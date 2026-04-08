47a762f19bd80cf9e8df9f8df739b84f1b0d5188
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-09 01:50:06 +0530

    feat: add tabulation method for Climbing Stairs variation

--- 46. DP (Part 01)/Climbing_Stairs_Variation.cpp
+++ 46. DP (Part 01)/Climbing_Stairs_Variation.cpp
@@ -35,6 +35,27 @@ int countWaysMem(int n, vector<int> &dp) {
     return dp[n];
 }
 
+
+//with Tabulation DP
+int countWaysTab(int n) {
+
+    if (n == 0 || n == 1) return 1;
+    if (n == 2) return 2;
+
+    vector<int> dp(n+1);
+
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i=3; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+
+    return dp[n];
+}
+
+
 int main() {
     int n = 4;
 
@@ -42,6 +63,8 @@ int main() {
 
     vector<int> dp(n+1, -1);
     cout << countWaysMem(n, dp) << endl; //7
+
+    cout << countWaysTab(n) << endl; //7
 
     return 0;
 }
