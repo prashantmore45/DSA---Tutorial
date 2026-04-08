cb5f9f48a79b90862aeb1915568e54f58a0ba96b
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-09 01:39:59 +0530

    feat: enhance Climbing Stairs variation with memoization method

--- 46. DP (Part 01)/Climbing_Stairs_Variation.cpp
+++ 46. DP (Part 01)/Climbing_Stairs_Variation.cpp
@@ -16,12 +16,32 @@ int countWaysRec(int n) {
     return countWaysRec(n-1) + countWaysRec(n-2) + countWaysRec(n-3);
 }
 
+//with memoization DP
+int countWaysMem(int n, vector<int> &dp) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    if (n == 2) {
+        return 2;
+    }
+
+    if (dp[n] != -1) {
+        return dp[n];
+    }
+
+    dp[n] = countWaysMem(n-1, dp) + countWaysMem(n-2, dp) + countWaysMem(n-3, dp);
+
+    return dp[n];
+}
 
 int main() {
     int n = 4;
 
     cout << countWaysRec(n) << endl; //7
 
-    
+    vector<int> dp(n+1, -1);
+    cout << countWaysMem(n, dp) << endl; //7
+
     return 0;
 }
