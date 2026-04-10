1e19d8c71c4d99c69a8e105dc54fa90d152e4fd3
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-10 23:58:37 +0530

    feat: add tabulation implementation for Longest Common Subsequence problem

--- 48. DP(Part 03)/Longest_Common_Subsequence.cpp
+++ 48. DP(Part 03)/Longest_Common_Subsequence.cpp
@@ -22,6 +22,7 @@ int lcsRec(string str1, string str2) {
     }
 }
 
+//Memoization
 
 int lcsMem(string str1, string str2, vector<vector<int>> dp) {
     int n = str1.size();
@@ -47,6 +48,27 @@ int lcsMem(string str1, string str2, vector<vector<int>> dp) {
     return dp[n][m];
 }
 
+//Tabulation
+
+int lcsTab(string str1, string str2) {
+    int n = str1.size();
+    int m = str2.size();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for (int i=1; i<n+1; i++) {
+        for (int j=1; j<m+1; j++) {
+            if (str1[i-1] == str2[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+
+    return dp[n][m];
+}
+
 
 int main() {
     string str1 = "abcdge";
@@ -60,6 +82,8 @@ int main() {
     cout << lcsRec(str1, str2) << endl; //4
 
     cout << lcsMem(str1, str2, dp) << endl; //4
+
+    cout << lcsTab(str1, str2) << endl; //4
 
     return 0;
 }
