d19536b0896a0fa85f1bc43bf5430b7e0a8ed5a8
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-10 23:48:30 +0530

    feat: add memoization implementation for Longest Common Subsequence problem

--- 48. DP(Part 03)/Longest_Common_Subsequence.cpp
+++ 48. DP(Part 03)/Longest_Common_Subsequence.cpp
@@ -23,11 +23,43 @@ int lcsRec(string str1, string str2) {
 }
 
 
+int lcsMem(string str1, string str2, vector<vector<int>> dp) {
+    int n = str1.size();
+    int m = str2.size();
+
+    if (n == 0 || m == 0) {
+        return 0;
+    }
+
+    if (dp[n][m] != -1) {
+        return dp[n][m];
+    }
+
+    if (str1[n-1] == str2[m-1]) {
+        dp[n][m] = lcsMem(str1.substr(0, n-1), str2.substr(0, m-1), dp) + 1;
+    } else {
+        int ans1 = lcsMem(str1.substr(0, n-1), str2, dp);
+        int ans2 = lcsMem(str1, str2.substr(0, m-1), dp);
+
+        dp[n][m] =  max(ans1, ans2);
+    }
+
+    return dp[n][m];
+}
+
+
 int main() {
     string str1 = "abcdge";
     string str2 = "abedg";
 
+    int n = str1.size();
+    int m = str2.size();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
     cout << lcsRec(str1, str2) << endl; //4
+
+    cout << lcsMem(str1, str2, dp) << endl; //4
 
     return 0;
 }
