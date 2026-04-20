a28e462e37982dbbea8af7290d241174db2df1a9
prashantmore45 <prashantmorepm05@gmail.com>
2026-04-20 22:48:58 +0530

    feat: add tabulation implementation for longest palindromic subsequence

--- 51. DP (Part 06)/Assignment/assignment6.cpp
+++ 51. DP (Part 06)/Assignment/assignment6.cpp
@@ -48,10 +48,39 @@ int longestPalindromeSubseqMem(string s) {
     return solveMem(s, 0, n - 1, dp);
 }
 
+//Tabulation
+
+int longestPalindromeSubseqTab(string s) {
+    int n = s.size();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    // Single characters
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 1;
+    }
+
+    // Length from 2 to n
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i <= n - len; i++) {
+            int j = i + len - 1;
+
+            if (s[i] == s[j]) {
+                dp[i][j] = 2 + dp[i + 1][j - 1];
+            } else {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
+
 int main() {
     string s = "bbbab";
     cout << longestPalindromeSubseqRec(s) << endl;
 
     cout << longestPalindromeSubseqMem(s) << endl;
+
+    cout << longestPalindromeSubseqTab(s) << endl;
     return 0;
 }
